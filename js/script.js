8b4edb73623841e9127ff11f26ab89c006333916
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-04-21 18:10:01 +0200

    Aggiunto evento Done alla stellina

--- js/script.js
+++ js/script.js
@@ -8,19 +8,23 @@ var app = new Vue(
             todos : [
                 {
                     name : 'Fare i compiti',
+                    isDone : false
                 },
                 {
                     name : 'Fare la spesa',
+                    isDone : false
                 },
                 {
-                    name : 'Fare il bucato'
+                    name : 'Fare il bucato',
+                    isDone : false
                 }
             ]
         },
         methods : {
             pushUserTodo() {
                 const userTodoItem = {
-                    name : this.userTodo
+                    name : this.userTodo,
+                    isDone : false
                 }
 
                 if( this.userTodo.length != '') {
@@ -30,6 +34,9 @@ var app = new Vue(
             },
             removeTodo(index) {
                 this.todos.splice(index, 1)
+            },
+            todoDone(index) {
+                this.todos[index].isDone = true;
             }    
         }
     }
