ede99045a823ac621ede36924a1155bca5a39c89
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-04-21 16:18:45 +0200

    Cercando di risolvere il bug

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var app = new Vue(
+    {
+        el : '#root',
+        data : {
+            // Creo un oggetto vuoto da far popolare all'input dell'utente
+            userTodo : {
+                name : ''
+            },
+            // Creo un Array di oggetti che contenga gli elementi da stampare nella ul
+            todos : [
+                {
+                    name : 'Fare i compiti',
+                },
+                {
+                    name : 'Fare la spesa',
+                },
+                {
+                    name : 'Fare il bucato'
+                }
+            ]
+        },
+        methods : {
+            pushUserTodo() {
+                this.todos.push(this.userTodo);
+            }
+        }
+    }
+)
